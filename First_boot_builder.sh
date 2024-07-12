52d9270b50b937909c3bac127cf2d2cf05294459
chaos0frenzy <31624634+chaos0frenzy@users.noreply.github.com>
2024-07-12 15:29:47 -0400

    Update First_boot_builder.sh

--- First_boot_builder.sh
+++ First_boot_builder.sh
@@ -2,7 +2,11 @@
 # 
 # sudo nano first_boot.sh
 #
-# sudo chmod +x /etc/profile.d/first_boot.sh
+# sudo chmod +x firstboot_setup.sh
+#
+# sudo cp first_boot.sh /etc/profile.d/
+#
+# sudo ./firstboot_setup.sh
 #
 #!/bin/bash
 
